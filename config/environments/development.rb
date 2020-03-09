494e943d6ee1cd7ea6e95d7a778e4ba179940401
Himmlers <himmler.fleur@gmail.com>
2020-03-09 11:03:15 +0100

    add devise

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -59,3 +59,6 @@ Rails.application.configure do
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 end
+
+#Action Mailer with Devise
+#config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -92,3 +92,6 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 end
+
+#Mailer Devise with Heroku
+#config.action_mailer.default_url_options = { :host => 'YOURAPPNAME.herokuapp.com' }
