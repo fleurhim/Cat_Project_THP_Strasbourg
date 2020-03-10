7593d8cee2238f3b12e6a06d067227f8a84275c8
nayow <nayow@hotmail.fr>
2020-03-10 15:07:25 +0100

    active storage instal

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,10 @@
 class User < ApplicationRecord
 	has_many :items, foreign_key: 'admin_id', class_name: "Item"
-	has_one :cart, foreign_key: 'customer_id', class_name: "Cart"
+  has_one :cart, foreign_key: 'customer_id', class_name: "Cart"
+  has_one_attached :avatar
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :items
+  root 'items#index'
   resources :users, only: [:show, :index] do
+    resources :avatars, only: [:create]
   	resources :carts, only: [:show]
   end
 end
