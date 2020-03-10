eb8c3300081729cd150d31689f4002599c191954
Mathieu <mathieu.farge@gmail.com>
2020-03-10 13:43:59 +0100

    add mailer

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,15 @@
 class User < ApplicationRecord
+	after_create :welcome_send
+
 	has_many :items, foreign_key: 'admin_id', class_name: "Item"
 	has_one :cart, foreign_key: 'customer_id', class_name: "Cart"
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  def welcome_send
+    UserMailer.welcome_email(self).deliver_now
+  end
+  
 end
