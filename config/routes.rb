e23f1968bf790ef9c654ee0387a8b75d137e35fb
Mathieu <mathieu.farge@gmail.com>
2020-03-11 16:04:02 +0100

    ajout controller et view de Charges

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
   root 'items#index'
   devise_for :users
+
   resources :items
+
   resources :users, only: [:show, :index] do
     resources :avatars, only: [:create]
     resources :carts, only: [:show]
   end
+
+  resources :charges
 end
