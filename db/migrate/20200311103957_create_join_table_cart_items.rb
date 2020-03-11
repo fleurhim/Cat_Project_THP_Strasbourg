705075e699982e56a21fb7d26682ebe24fd9089d
nayow <nayow@hotmail.fr>
2020-03-11 15:51:54 +0100

    created orders table+model

--- db/migrate/20200311103957_create_join_table_cart_items.rb
+++ db/migrate/20200311103957_create_join_table_cart_items.rb
@@ -2,7 +2,7 @@ class CreateJoinTableCartItems < ActiveRecord::Migration[5.2]
   def change
     create_table :join_table_cart_items do |t|
       t.belongs_to :cart, index: true
-      t.belongs_to :item, index: true
+      t.belongs_to :customer, index: true
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_11_124311) do
+ActiveRecord::Schema.define(version: 2020_03_11_141419) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -61,6 +61,18 @@ ActiveRecord::Schema.define(version: 2020_03_11_124311) do
     t.datetime "updated_at", null: false
     t.index ["cart_id"], name: "index_join_table_cart_items_on_cart_id"
     t.index ["item_id"], name: "index_join_table_cart_items_on_item_id"
+  end
+
+  create_table "orders", force: :cascade do |t|
+    t.string "delivery_address"
+    t.integer "items_number"
+    t.float "total_order"
+    t.bigint "cart_id"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["cart_id"], name: "index_orders_on_cart_id"
+    t.index ["user_id"], name: "index_orders_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
