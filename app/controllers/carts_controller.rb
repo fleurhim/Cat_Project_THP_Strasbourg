5c51cda374d6f1d4b78a611df270e1d46a0c09eb
JadeCaillot <jadeca@wanadoo.fr>
2020-03-11 15:22:16 +0100

    navbar + cart_show done

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,6 +1,6 @@
 class CartsController < ApplicationController 
     def show
-      @cart=Cart.find_by(customer_id: "23")
+      @cart=Cart.find_by(customer_id: "10")
       @items=@cart.items
     end
 end
