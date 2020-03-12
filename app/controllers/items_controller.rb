d19f31e6c0fb92983174c1d6e38917e7e7c523e0
JadeCaillot <jadeca@wanadoo.fr>
2020-03-12 15:39:39 +0100

    item#create + item#new

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -8,4 +8,19 @@ class ItemsController < ApplicationController
 		@item = Item.find(params[:id])
 	end
 
+	def new
+		@item=Item.new
+	end 
+
+	def create
+		@item=Item.new(title: params[:title], description: params[:description], price: params[:price], image_url: params[:image_url], admin: current_user)
+		if @item.save
+			redirect_to item_path(@item.id)
+			flash[:success] = "This kitty has been successfully added to the kitty database"
+		else
+			flash[:error] = "Something went wrong, please try again"
+			render :new
+		end
+	end
+
 end
